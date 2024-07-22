536f99516d9a3fc093d1177f6de0e414c3f81754
jeremiahmushtaq <jerrymushtaq83@gmail.com>
2024-07-22 20:07:13 +0100

    Evaluation metrics saving features
    Added code to save evaluation metrics in graphical and tabular formats.

--- cnn.py
+++ cnn.py
@@ -1,9 +1,10 @@
 import os
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.models import Sequential 
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Input
+from tensorflow.keras.models import Sequential  # type: ignore
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense # type: ignore
 from matplotlib import pyplot as plt
+import pandas as pd
 
 """
 Setup and Loading
@@ -27,14 +28,17 @@ def load_haplotypes(directory):
     return haplotypes
 
 # Set directories' path
-high_dir = '/Users/jeremiahmushtaq/Documents/University/MSc Research Project/Simulation Results/Rep 1/High'
-medium_dir = '/Users/jeremiahmushtaq/Documents/University/MSc Research Project/Simulation Results/Rep 1/Medium'
-low_dir = '/Users/jeremiahmushtaq/Documents/University/MSc Research Project/Simulation Results/Rep 1/Low'
+high_dir = '/Users/jeremiahmushtaq/Documents/University/MSc Research Project/Simulation Results/Rep 1 (100)/High'
+medium_dir = '/Users/jeremiahmushtaq/Documents/University/MSc Research Project/Simulation Results/Rep 1 (100)/Medium'
+low_dir = '/Users/jeremiahmushtaq/Documents/University/MSc Research Project/Simulation Results/Rep 1 (100)/Low'
 
 # Load haplotypes
 high = load_haplotypes(high_dir)
 medium = load_haplotypes(medium_dir)
 low = load_haplotypes(low_dir)
+
+# User status update
+print("Completed Setup & Loading.")
 
 """
 Data Preprocessing
@@ -43,8 +47,8 @@ Data Preprocessing
 min_hap_len_high = min(len(j) for i in high for j in i)
 min_hap_len_medium = min(len(j) for i in medium for j in i)
 min_hap_len_low = min(len(j) for i in low for j in i)
-min_hap_len = min(min_hap_len_high, min_hap_len_medium, min_hap_len_high)
-print(min_hap_len) # 4882
+min_hap_len = min(min_hap_len_high, min_hap_len_medium, min_hap_len_low)
+print("Minimum Sequence Length:", min_hap_len)
 
 # Define function to truncate sequences
 def truncate(category):
@@ -64,6 +68,9 @@ low_hap_trunc = np.array(truncate(low))
 
 # Build dataset
 data = np.concatenate((high_hap_trunc, med_hap_trunc, low_hap_trunc), axis=0)
+
+# User status update
+print("Completed Data Preprocessing.")
 
 """
 Build Model
@@ -95,14 +102,52 @@ model = Sequential([
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
 # Fit model
-history = model.fit(X, y, epochs=10, batch_size=32, validation_split=0.2)
+hist = model.fit(X, y, epochs=10, batch_size=16, validation_split=0.2)
+
+# User status update
+print("Designed and fitted model.")
 
 """
-Training & Validation Evaluation
+Save Tabular Metrics
 """
+# File logistics
+output_directory = '/Users/jeremiahmushtaq/Documents/University/MSc Research Project/Evaluation Logs/Tables/Multilayer Testing'
+output_file_name = 'metrics.tsv'
+metrics_file_path = os.path.join(output_directory, output_file_name)
+
+# Create dataframe with metrics
+df = pd.DataFrame({
+    'Epoch (%)': range(1, len(hist.history['accuracy']) + 1),
+    'Training Accuracy (%)': hist.history['accuracy'],
+    'Validation Accuracy (%)': hist.history['val_accuracy'],
+    'Training Loss (%)': hist.history['loss'],
+    'Validation Loss (%)': hist.history['val_loss']
+})
+
+# Format values
+df['Training Accuracy (%)'] = df['Training Accuracy (%)'].apply(lambda x: f"{x * 100:.2f}")
+df['Validation Accuracy (%)'] = df['Validation Accuracy (%)'].apply(lambda x: f"{x * 100:.2f}")
+df['Training Loss (%)'] = df['Training Loss (%)'].apply(lambda x: f"{x:.2f}")
+df['Validation Loss (%)'] = df['Validation Loss (%)'].apply(lambda x: f"{x:.2f}")
+
+# Save the dataframe to a tsv file
+df.to_csv(metrics_file_path, sep='\t', index=False)
+print("Results saved as", output_file_name)
+
+# User status update
+print("Tabular metrics saved.")
+
+"""
+Save Graphical Metrics
+"""
+# Define the directory and file name
+output_directory = '/Users/jeremiahmushtaq/Documents/University/MSc Research Project/Evaluation Logs/Images/Multilayer Testing'
+output_file_name = 'plot.png'
+metrics_file_path = os.path.join(output_directory, output_file_name)
+
 plt.subplot(1, 2, 1)
-plt.plot(history.history['accuracy'])
-plt.plot(history.history['val_accuracy'])
+plt.plot(hist.history['accuracy'])
+plt.plot(hist.history['val_accuracy'])
 plt.title('Model accuracy')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
@@ -110,12 +155,17 @@ plt.legend(['Train', 'Validation'], loc='upper left')
 
 # Plot training & validation loss values
 plt.subplot(1, 2, 2)
-plt.plot(history.history['loss'])
-plt.plot(history.history['val_loss'])
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
 plt.title('Model loss')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Validation'], loc='upper right')
-
 plt.tight_layout()
-plt.show()
+
+# Save the plot
+plt.savefig(metrics_file_path)
+print("Results saved as", output_file_name)
+
+# User status update
+print("Graphical metrics saved.")
